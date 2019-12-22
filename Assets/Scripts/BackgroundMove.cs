74671c0e545358ab790cda540cc8780f8dccd64f
Oscar Shiang <oscar0225@livemail.tw>
2019-12-22 16:29:17 +0000

    add pause scene

--- Assets/Scripts/BackgroundMove.cs
+++ Assets/Scripts/BackgroundMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BackgroundMove : MonoBehaviour {
 
     public Rigidbody2D player;
+    public GameObject pause;
+
 
     public float rate;
     private Rigidbody2D rb;
@@ -13,11 +15,16 @@ public class BackgroundMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+
+        pause.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float velocity = player.velocity.x * rate;
         rb.velocity = new Vector3(velocity, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            pause.SetActive(!pause.activeSelf);
 	}
 }
---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseButton : MonoBehaviour, IPointerClickHandler {
+
+    public GameObject pause;
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        pause.SetActive(!pause.activeSelf);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+};
