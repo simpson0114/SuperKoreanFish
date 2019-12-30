2c728c9d7cfd394c7ff83e7420245c52281e8a6e
Oscar Shiang <oscar0225@livemail.tw>
2019-12-30 07:59:29 +0000

    repair question block

--- Assets/Scripts/QuestionBlock.cs
+++ Assets/Scripts/QuestionBlock.cs
@@ -8,29 +8,37 @@ public class QuestionBlock : MonoBehaviour {
     private Animator anime;
     private Rigidbody2D rb;
 
+    bool used;
+
 	// Use this for initialization
 	void Start () {
         anime = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 
         pos = transform.position;
+
+        used = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y <= pos.y)
+		if (transform.position.y < pos.y && used)
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        if (!used && transform.position.y < pos.y)
         {
-            rb.velocity = new Vector3(0, 0, 0);
+            rb.velocity = Vector3.zero;
             transform.position = pos;
         }
 	}
 
     void OnCollisionEnter2D(Collision2D c)
     {
-        if (c.collider.tag == "Player")
+        if (c.collider.tag == "Player" && !used)
         {
             rb.velocity = new Vector3(0, 0.5f, 0);
             anime.SetBool("used", true);
+
+            used = true;
         }
     }
 }
---
+++ Assets/Scripts/platformControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformControl : MonoBehaviour {
+
+    [Header("Boundary Setting")]
+    public float left;
+    public float right;
+
+    [Header("Move Setting")]
+    public float stepSize;
+
+    Vector3 des;
+
+	// Use this for initialization
+	void Start () {
+        des = transform.position;
+        des.x = right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, des, Time.deltaTime * stepSize);
+        if (transform.position.x >= right)
+            des.x = left;
+        else if (transform.position.x <= left)
+            des.x = right;
+	}
+}
