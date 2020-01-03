134ba233ab40fb561e5c294e62c1a58791fade5b
Oscar Shiang <oscar0225@livemail.tw>
2020-01-03 01:26:52 +0000

    test script

---
+++ Assets/Scripts/DataReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class DataReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+        Save save = (Save)bf.Deserialize(file);
+        file.Close();
+
+        Debug.Log("progress " + save.progress);
+        Debug.Log("item count");
+        for (int i = 0; i < 4; i ++)
+        {
+            Debug.Log(save.item[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
